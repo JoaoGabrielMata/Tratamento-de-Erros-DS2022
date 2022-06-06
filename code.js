61ab750d5c6aa86fb2d96011fbe09cb1024bf6a2
JoaoGabrielMata <joaogabrielmta@gmail>
2022-06-06 20:44:22 -0300

    Correção adicional

--- code.js
+++ code.js
@@ -4,20 +4,17 @@ inputFileButton.addEventListener("click", clickFileButton)
 const NumberButton = document.getElementById("inputNumberButton")
 inputNumberButton.addEventListener("click", clickNumberButton)
 
-function clickFileButton(){
-}
+function clickFileButton() {}
 
-function clickNumberButton(){
-number = document.getElementById("inputNumber").value
-try{
-   if(number == "") throw 'Informe um valor'
-   if(number <5 || number>10) throw 'Informe um valor entre 5 e 10'
-   document.getElementById("outputNumber"). innerHTML = "o número escolhido é " +number
-}
-catch (erro){
-  document.getElementById("outputNumber").innerHTML = erro
-}
-finally{
-  document.getElementById("outputFinal").innerHTML = "Continue o Cadastro corretamente!"
-}
+function clickNumberButton() {
+  number = document.getElementById("inputNumber").value
+  try {
+    if (number == "") throw 'Informe um valor'
+    if (number < 5 || number > 10) throw 'Informe um valor entre 5 e 10'
+    document.getElementById("outputNumber").innerHTML = "o número escolhido é " + number
+  } catch (erro) {
+    document.getElementById("outputNumber").innerHTML = erro
+  } finally {
+    document.getElementById("outputFinal").innerHTML = 'Continue o cadastro corretamente!'
+  }
 }
